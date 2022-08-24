94ef427ad0bc8ce947804cb574af1492669797ae
Sagleft <sagleft@inbox.ru>
2022-08-24 12:35:06 +0300

    update contact message

--- messages.go
+++ messages.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	utopiago "github.com/Sagleft/utopialib-go"
 )
 
 func (app *solution) onContactMessage(m utopiago.InstantMessage) {
-	fmt.Println("[CONTACT] " + m.Nick + ": " + m.Text) // placeholder
+	fmt.Println("[spy]")
 }
 
 func (app *solution) onChannelMessage(m utopiago.ChannelMessage) {
@@ -16,20 +15,7 @@ func (app *solution) onChannelMessage(m utopiago.ChannelMessage) {
 		return
 	}
 
-	if strings.ToLower(m.Text) == "crypton rate" {
-		rate, err := app.getCryptonPrice()
-		if err != nil {
-			app.onError(err)
-			return
-		}
-
-		msg := "1 [diamond] = " + formatFloat(float64(rate), 2) + "$"
-		_, err = app.Config.Utopia.SendChannelMessage(m.ChannelID, msg)
-		if err != nil {
-			app.onError(err)
-			return
-		}
-	}
+	// TODO
 }
 
 func (app *solution) onPrivateChannelMessage(m utopiago.ChannelMessage) {
